3b93b60ce59545ba40dce68cc7b21d119fda76f4
caraballova <vanessa.caraballo@bryanuniversity.edu>
2020-11-23 22:11:18 -0500

    submit_events

--- week5/submit_events.js
+++ week5/submit_events.js
@@ -1,10 +1,14 @@
-var submit = document.getElementById("submit");
+/* var submit = document.getElementById("submit");
 var name = document.getElementById("name");
 var email = document.getElementById("email");
-var num = document.getElementById("num");
+var num = document.getElementById("num"); */
 
 
 submit.addEventListener("click", function(){
+    var submit = document.getElementById("submit");
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var num = document.getElementById("num");
     alert("Name: " + name.value + "\nEmail: " + email.value + "\nPhone Number: " + num.value);
 });
 
